749b201360aac8a91748163b480ec3136733426e
AzureAD\박솔민 <bowwow1994@hanmail.net>
2021-12-05 10:11:03 +0900

    Add post api

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 from api.login import Login 
+from api.post import Post 
 
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Login, '/login')
+api.add_resource(Post, '/post')
 api.init_app(app)
 if __name__ == '__main__':
     app.run(debug=True)
